648051c50b4979477331cab4d7861be45ea72e56
Arin0303 <cindy030225@gmail.com>
2025-05-23 14:22:25 +0900

    dtos폴더 추가

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ app.use(express.static('public'));          // 정적 파일 접근
 app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
 
+app.post('/api/register', (req, res) => {
+  
+});
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
